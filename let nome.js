b9bef8a7749abfc936369b7f6e9e5d02d0f4bd07
Renata Claudino <saraiva.claudino@gmail.com>
2025-06-18 20:44:36 -0300

    testes realizados no console

--- let nome.js
+++ let nome.js
@@ -1,1 +1,39 @@
-let nome = "Renata"
+let nome = "Renata";
+let saudacao = "Hello World!";
+
+
+//usados no console
+let nome = "Renata";
+undefined
+let idade = "32";
+undefined
+let altura = "1.77";
+undefined
+let altura = 1.77;
+undefined
+let estudos == true
+VM967:1 Uncaught SyntaxError: Unexpected token '=='Entenda o erro
+let estudos = true
+undefined
+console.log("Meu nome é" nome);
+VM1103:1 Uncaught SyntaxError: missing ) after argument listEntenda o erro
+console.log("Meu nome é:" nome)
+VM1166:1 Uncaught SyntaxError: missing ) after argument listEntenda o erro
+console.log("Meu nome é: " nome);
+VM1268:1 Uncaught SyntaxError: missing ) after argument listEntenda o erro
+let nomeCompleto = "Renata" ;
+undefined
+console.log("Meu nome é: ", nomeCompleto);
+VM1508:1 Meu nome é:  Renata
+undefined
+console.log ("Minha idade é", 2025-1993)
+VM1646:1 Minha idade é 32
+undefined
+console.log("Estou estudando no momento?", true)
+VM1776:1 Estou estudando no momento? true
+undefined
+let estouEstudando = true;
+undefined
+console.log("Estou estudando no moemento?", estouEstudando)
+VM2005:1 Estou estudando no moemento? true
+undefined
